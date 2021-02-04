476acafccd3f3006efc9a733968b7de6b7e3afd6
steve-bush <stevejr@bushinc.com>
2021-02-04 14:54:18 -0600

    Scrapes par from espn now and minor improvements

--- golf.py
+++ golf.py
@@ -1,40 +1,29 @@
 import pandas as pd
+from bs4 import BeautifulSoup
+import requests
 
-rounds = 4
-par = 72
-filename = 'players.csv'
+# Variables to be set by the user
+url = 'https://www.espn.com/golf/leaderboard' # Website used for scores and par
+filename = 'players.csv'                      # csv file in name,player,player... format
+rounds = 4                                    # Number of rounds to use in when calculating the score
 
-# Scrape espn
-table = pd.read_html('https://www.espn.com/golf/leaderboard')
+# Parse espn for par
+html_content = requests.get(url).text
+soup = BeautifulSoup(html_content, 'lxml')
+par = int(soup.find(class_='Leaderboard__Course__Location__Detail n8 clr-gray-04').get_text()[3:5])
+
+# Scrape espn leaderboard
+table = pd.read_html(url)
 results = table[0]
-# Drop uneeded data
-results = results.drop(['POS', 'TO PAR', 'TOT', 'EARNINGS', 'FEDEX PTS'], axis=1)
+
 # Create a dictionary of lists of columns
 results_dict = results.to_dict(orient='list')
 
-# Create a dictionary of golfer scores
-golfer_scores = {}
+# Clean the (a) from player names if needed
 for i in range(len(results_dict['R1'])):
     name = results_dict['PLAYER'][i]
     if ' (a)' in name:
-        name = name[:-4]
-    r1 = int(results_dict['R1'][i]) if results_dict['R1'][i] != '--' else 0
-    r2 = int(results_dict['R2'][i]) if results_dict['R2'][i] != '--' else 0
-    r3 = int(results_dict['R3'][i]) if results_dict['R3'][i] != '--' else 0
-    r4 = int(results_dict['R4'][i]) if results_dict['R4'][i] != '--' else 0
-    scores = [r1, r2, r3, r4]
-    score = 0
-    for j in range(rounds):
-        # If player has made the cut
-        if scores[j] != 0:
-            # Add the par score
-            score += scores[j] - par
-        # If player missed the cut
-        else:
-            # Add the worst par score
-            score += max(scores) - par
-    # Add score and golfer to dictionary
-    golfer_scores[name] = score
+        results_dict['PLAYER'][i] = name[:4]
 
 # Read in player info
 player_golfers = {}
@@ -50,7 +39,23 @@ player_scores = []
 for player, golfers in player_golfers.items():
     player_score = 0
     for golfer in golfers:
-        player_score += golfer_scores[golfer]
+        # Find index of the golfer in each list
+        i = results_dict['PLAYER'].index(golfer)
+        # Put the round scores into a usable format
+        r1 = int(results_dict['R1'][i]) if results_dict['R1'][i] != '--' else 0
+        r2 = int(results_dict['R2'][i]) if results_dict['R2'][i] != '--' else 0
+        r3 = int(results_dict['R3'][i]) if results_dict['R3'][i] != '--' else 0
+        r4 = int(results_dict['R4'][i]) if results_dict['R4'][i] != '--' else 0
+        scores = [r1, r2, r3, r4]
+        for j in range(rounds):
+            # If player has made the cut
+            if scores[j] != 0:
+                # Add the par score
+                player_score += scores[j] - par
+            # If player missed the cut
+            else:
+                # Add the worst par score
+                player_score += max(scores) - par
     players.append(player)
     player_scores.append(player_score)
 
